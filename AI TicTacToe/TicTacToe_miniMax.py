86fb6b6d2e9e47f53b7e0cba1c9b8c28f6a471ee
mSakib20 <msakib2022@fau.edu>
2023-06-04 17:51:32 -0400

    implemented winning and draw fucntion

--- AI TicTacToe/TicTacToe_miniMax.py
+++ AI TicTacToe/TicTacToe_miniMax.py
@@ -24,7 +24,7 @@ def insertLetter(playerMove, position):
         tBoard[position] = playerMove
         print(tBoard)
 
-        #Now checking if the move makes it a draw
+        #Now checking if the move makes it a draw/win 
         if drawChecker():
             print("It's a DRAW!!")
             exit()
@@ -38,11 +38,42 @@ def insertLetter(playerMove, position):
         else:
             print("ERROR!!")
             print("Check your insertLetter():")
-        return
+            return
     else:
         print("Invalid MOVE!!")
-    return
+        print("\n")
+
+        postion = int(input("Please enter a new postion this time!"))
+        insertLetter(playerMove, position)
+        return
         
+#Cheking to see who wins the game X or O 
+def winnerChecker():
+    if (tBoard[1] == tBoard[2] and tBoard[1] == tBoard[3] and tBoard[1] != ' '):
+        return True
+    elif (tBoard[4] == tBoard[5] and tBoard[4] == tBoard[6] and tBoard[4] != ' '):
+        return True
+    elif (tBoard[7] == tBoard[8] and tBoard[7] == tBoard[9] and tBoard[7] != ' '):
+        return True
+    elif (tBoard[1] == tBoard[4] and tBoard[1] == tBoard[7] and tBoard[1] != ' '):
+        return True
+    elif (tBoard[2] == tBoard[5] and tBoard[2] == tBoard[8] and tBoard[2] != ' '):
+        return True
+    elif (tBoard[3] == tBoard[6] and tBoard[3] == tBoard[9] and tBoard[3] != ' '):
+        return True
+    elif (tBoard[1] == tBoard[5] and tBoard[1] == tBoard[9] and tBoard[1] != ' '):
+        return True
+    elif (tBoard[7] == tBoard[5] and tBoard[7] == tBoard[3] and tBoard[7] != ' '):
+        return True
+    else:
+        return False
+
+#Cheking to see if the game is DRAW  
+def drawChecker():
+    for key in tBoard.keys():
+        if tBoard[key] == ' ':
+            return False
+    return True
 
 
 
