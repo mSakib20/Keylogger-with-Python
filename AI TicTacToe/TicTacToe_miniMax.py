36b660f43751a20df5827cf0beccc3b97890ec48
mSakib20 <msakib2022@fau.edu>
2023-06-04 17:29:51 -0400

    set up the insert Letter fuction for bot/human

--- AI TicTacToe/TicTacToe_miniMax.py
+++ AI TicTacToe/TicTacToe_miniMax.py
@@ -13,4 +13,43 @@ def printBoard(tBoard):
     print(tBoard[7] + "|" + tBoard[8] + "|" + tBoard[9])
     print("\n")
 
+#Checking if the selected position/space in the board is free
+def freeSpaceChecker(postion):
+    if tBoard(postion) == ' ':
+        return True
+    return False
+
+def insertLetter(playerMove, position):
+    if freeSpaceChecker(position) == ' ':
+        tBoard[position] = playerMove
+        print(tBoard)
+
+        #Now checking if the move makes it a draw
+        if drawChecker():
+            print("It's a DRAW!!")
+            exit()
+        elif winnerChecker():
+            if playerMove == 'X':
+                print("Computer WINS!!")
+                exit()
+            else:
+                print("Did you really just beat the AI??")
+                exit()
+        else:
+            print("ERROR!!")
+            print("Check your insertLetter():")
+        return
+    else:
+        print("Invalid MOVE!!")
+    return
+        
+
+
+
+
+
+
+
+
 printBoard(tBoard)
+
