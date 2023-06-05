4a2b154545f99921f426bb57f1596afd0c354e12
mSakib20 <msakib2022@fau.edu>
2023-06-05 03:14:26 -0400

    implemented minimax algorithm

--- AI TicTacToe/TicTacToe_miniMax.py
+++ AI TicTacToe/TicTacToe_miniMax.py
@@ -19,12 +19,11 @@ def printBoard(tBoard):
 def freeSpaceChecker(position):
     if tBoard[position] == ' ':
         return True
-    else:
-        return False
+    return False
 
-def insertLetter(playerMove, position):
+def insertLetter(letter, position):
     if freeSpaceChecker(position):
-        tBoard[position] = playerMove
+        tBoard[position] = letter
         printBoard(tBoard)
 
         #Now checking if the move makes it a draw/win 
@@ -32,7 +31,7 @@ def insertLetter(playerMove, position):
             print("It's a DRAW!!")
             exit()
         if winnerChecker():
-            if playerMove == 'X':
+            if letter == 'X':
                 print("Computer WINS!!")
                 exit()
             else:
@@ -43,8 +42,8 @@ def insertLetter(playerMove, position):
         print("Invalid MOVE!!")
         print("\n")
 
-        postion = int(input("Please enter a new postion this time!"))
-        insertLetter(playerMove, position)
+        position = int(input("Please enter a new postion this time!"))
+        insertLetter(letter, position)
         return
         
 #Cheking to see who wins the game X or O 
@@ -67,6 +66,27 @@ def winnerChecker():
         return True
     else:
         return False
+    
+#Cheking to see who wins the game X or O (backgroud check for the AI to decide the best move)
+def winner(mark):
+    if (tBoard[1] == tBoard[2] and tBoard[1] == tBoard[3] and tBoard[1] == mark):
+        return True
+    elif (tBoard[4] == tBoard[5] and tBoard[4] == tBoard[6] and tBoard[4] == mark):
+        return True
+    elif (tBoard[7] == tBoard[8] and tBoard[7] == tBoard[9] and tBoard[7] == mark):
+        return True
+    elif (tBoard[1] == tBoard[4] and tBoard[1] == tBoard[7] and tBoard[1] == mark):
+        return True
+    elif (tBoard[2] == tBoard[5] and tBoard[2] == tBoard[8] and tBoard[2] == mark):
+        return True
+    elif (tBoard[3] == tBoard[6] and tBoard[3] == tBoard[9] and tBoard[3] == mark):
+        return True
+    elif (tBoard[1] == tBoard[5] and tBoard[1] == tBoard[9] and tBoard[1] == mark):
+        return True
+    elif (tBoard[7] == tBoard[5] and tBoard[7] == tBoard[3] and tBoard[7] == mark):
+        return True
+    else:
+        return False
 
 #Cheking to see if the game is DRAW  
 def drawChecker():
@@ -85,11 +105,63 @@ def computerPlayerMove():
     insertLetter(computer, position)
     return
 
+#Code for the AI to calulate the best move for the node
+def aiMove():
+    bestScore = -900
+    bestMove = 0
+                
+    for key in tBoard.keys():                       #looping through all the empty spaces inside the dictionary and calculating the minimax for each key
+        if tBoard[key] == ' ':
+            tBoard[key] = computer    
+            score = miniMax(tBoard, False)
+            tBoard[key] == ' '                      #after getting the value for 'score' replacing the key with an empty string
+            if score > bestScore:                   
+                bestScore = score                   #keeping track of the 'bestScore'
+                bestMove = key                      #setting the currect key as the postion for the 'tBoard'
+    insertLetter(computer, bestMove)                #inserting computer letter (X) in the board at the place which may lead to the victory
+    return
+
+def miniMax(tBoard, maximizing):
+    if winner(computer):
+        return 1
+    elif winner(player):
+        return -1
+    elif drawChecker():
+        return 0
+    
+    if maximizing:
+        bestScore = -900
+
+        for key in tBoard.keys():
+            if tBoard[key] == ' ':
+                tBoard[key] = computer
+                score = miniMax(tBoard, False)
+                tBoard[key] = ' '
+                
+                if score > bestScore:
+                    bestScore = score
+        return bestScore
+    
+    else:
+        bestScore = 900
+        for key in tBoard.keys():
+            if tBoard[key] == ' ':
+                tBoard[key] = player
+                score = miniMax(tBoard, True)
+                tBoard[key] = ' '
+                
+                if score < bestScore:
+                    bestScore = score
+        return bestScore
+    
+
 #Driver starts here
 print("\n")
 printBoard(tBoard)
 
 #Creating a game loop
 while not winnerChecker():
-    computerPlayerMove()
+    
+    #aiMove()
     humanPlayerMove()
+    computerPlayerMove()
